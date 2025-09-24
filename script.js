333acf778a73586ab25929078dd9de7a869c0378
Maëlle Rabouan <maelle.rabouan@gmail.com>
2025-09-24 17:19:38 +0200

    feat(): mini formations slider  wip

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const emblaNode = document.querySelector(".embla");
+const embla2Node = document.querySelector(".embla2");
 const dotsNode = document.querySelector(".embla__dots");
 const options = { loop: true };
 const plugins = [EmblaCarouselAutoplay()];
 const emblaApi = EmblaCarousel(emblaNode, options, plugins);
+const embla2Api = EmblaCarousel(embla2Node, options);
 
 // console.log(emblaApi.slideNodes()); // Access API
 
@@ -27,3 +29,9 @@ dots.forEach((dot, i) =>
 
 emblaApi.on("select", setSelectedDot);
 setSelectedDot();
+
+// Slider 2 button
+const prevBtnNode = embla2Node.querySelector(".embla2__button--prev");
+const nextBtnNode = embla2Node.querySelector(".embla2__button--next");
+prevBtnNode.addEventListener("click", () => embla2Api.scrollPrev());
+nextBtnNode.addEventListener("click", () => embla2Api.scrollNext());
